ec5859433086f32edc7091d79fc15ed393b7505b
marlysco <marlysco94@gmail.com>
2021-06-14 10:40:30 -0400

    Updating Services.js Model

--- models/Services.js
+++ models/Services.js
@@ -1,4 +1,4 @@
-const { Model, DataTypes, DECIMAL } = require('sequelize');
+const { Model, DataTypes} = require('sequelize');
 const sequelize = require('../config/connection');
 
 class Services extends Model {}
