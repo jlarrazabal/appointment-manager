b0075e595e2e33d933dd8df844fbde30cf2b1bb3
jlarrazabal <69408299+jlarrazabal@users.noreply.github.com>
2021-06-14 21:55:11 -0400

    Adding home route

--- models/Appointment.js
+++ models/Appointment.js
@@ -7,70 +7,69 @@ const sequelize = require('../config/connection');
 class Appointment extends Model {}
 
 Appointment.init({
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      service_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'service',
-          key: 'id',
-        },
-      },
-      payment_id: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      date: {
-        type: DataTypes.DATEONLY,
-        allowNull: false,
-        validate: {
-          isDate: true
-        },
-      },
-      day: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'calendar',
-          key: 'day',
-        },
-      },
-      hour: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        references: {
-          model: 'calendar',
-          key: 'hour',
-        },
-      },
-      user_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'user',
-          key: 'id',
-        },
-        calendar_id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          references: {
-            model: 'calendar',
-            key: 'id',
-          },
-        },
-      },
-      {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'appointment',
-      }
-    );
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  service_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'service',
+      key: 'id',
+    },
+  },
+  payment_id: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  date: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+    validate: {
+      isDate: true
+    },
+  },
+  day: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'calendar',
+      key: 'day',
+    },
+  },
+  hour: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    references: {
+      model: 'calendar',
+      key: 'hour',
+    },
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'user',
+      key: 'id',
+    }
+  },
+  calendar_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'calendar',
+      key: 'id',
+    },
+  },
+}, {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'appointment',
+});
 
-    module.exports = Appointment;
+module.exports = Appointment;
