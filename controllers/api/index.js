c7100f881b6a8962a7fd7fb6553eb118be62b70e
marlysco <marlysco94@gmail.com>
2021-06-18 00:58:02 -0400

    Updating new appointments

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const serviceRoutes= require('./serviceRoutes');
+const appointmentRoutes = require('./appointmentRoutes');
 
 router.use('/users', userRoutes);
 router.use('/service', serviceRoutes);
+router.use('/appointment', appointmentRoutes);
+
+
 
 module.exports = router;
