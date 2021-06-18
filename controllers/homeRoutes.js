911b4c2f89f72ce6162ca72bc7311b412701daa1
jlarrazabal <69408299+jlarrazabal@users.noreply.github.com>
2021-06-18 00:00:17 -0400

    Working on Admin Homepage Route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,12 +1,17 @@
 const router = require('express').Router();
-const {User, Appointment, Service, Calendar} = require('../models');
+const {
+  User,
+  Appointment,
+  Service,
+  Calendar
+} = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get("/", async (req, res) => {
   try {
     const servicesData = await Service.findAll({});
-    if(!servicesData.length){
-      if(!req.session.logged_in) {
+    if (!servicesData.length) {
+      if (!req.session.logged_in) {
         const response = {
           about_us: true,
           business_hours: true
@@ -22,17 +27,23 @@ router.get("/", async (req, res) => {
         res.render("notFound", response);
       }
     } else {
-      const services = servicesData.map(service => service.get({plain: true}));
-      if(!req.session.logged_in) {
+      const services = servicesData.map(service => service.get({
+        plain: true
+      }));
+      if (!req.session.logged_in) {
         const response = {
-          services: {services},
+          services: {
+            services
+          },
           about_us: true,
           business_hours: true
         }
         res.render("homepage", response);
       } else {
         const response = {
-          services: {services},
+          services: {
+            services
+          },
           about_us: true,
           business_hours: true,
           logged_in: req.session.logged_in,
@@ -41,11 +52,10 @@ router.get("/", async (req, res) => {
         res.render("homepage", response);
       }
     }
-  } catch(err) {
+  } catch (err) {
     res.status(500).json(err);
   }
 });
-
 
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
@@ -55,44 +65,42 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-router.get("/lady-lash-admin-homepage", async (req, res) => {
+router.get("/lady-lash-admin-homepage", withAuth, async (req, res) => {
   try {
-    const appointmentsData = await Appointment.findAll({
-      include: [{
-        model: "user",
-        attributes: ["id","first_name","last_name","appoiments_counter"]
-      },
-    {
-      model: "service",
-      attributes: ["id","name"]
-    },
-    {
-      model: "calendar",
-      attributes: ["id","day","hour","start_date","end_date"]
-    }],
-      where: {date: req.body.date}
-    });
-    const appointments = appointmentsData.map(appointment => appointment.get({plain: true}));
-    res.status(200).json({appointments});
-  } catch(err) {
+    const date = "2021-06-21";
+    const appointmentsData = await Appointment.findAll({});
+
+    if (!appointmentsData.length) {
+      const availability = {
+        h08: false,
+        h10: false,
+        h12: false,
+        h14: false,
+        h16: false,
+        h18: false,
+        h20: false
+      };
+      res.render("adminHomepage", {availability: availability, logged_in: req.session.logged_in, user_id: req.session.user_id});
+    } else {
+      const appointments = appointmentsData.map(appointment => appointment.get({
+        plain: true
+      }));
+      res.send({appointments});
+    }
+  } catch (err) {
     res.status(500).json(err);
   }
 });
 
-//avaliabiliy  -GET
+
 router.get("/avaliability", async (req, res) => {
   try {
-    const calenderData = await Calender.findAll({});
+    const appointmentData = await Appointment.findAll({});
       if(!req.session.logged_in) {
-        const response = {
-          avaliability: true,
-        }
-        res.render("notFound", response);
+       //render avaliability page
+        res.render("avaliability");
     } else {
-      const response = {
-        make_appointment: true,
-      }
-        res.render("homepage", response);
+        res.redirect("/create-appointment");
       }
   } catch(err) {
     res.status(500).json(err);
