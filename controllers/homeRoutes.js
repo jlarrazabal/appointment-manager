a12895434717726372195ad58b1f77d2003e7140
jlarrazabal <69408299+jlarrazabal@users.noreply.github.com>
2021-06-20 23:21:09 -0400

    Complete the Create Appointment Route, My Appointments Route, and User Appointments view
    Complete the Create Appointment Route, My Appointments Route, and User Appointments view.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -9,7 +9,7 @@ const withAuth = require('../utils/auth');
 
 
 //Route to Get to the Homepage
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const servicesData = await Service.findAll({});
     if (!servicesData.length) {
@@ -68,7 +68,7 @@ router.get('/login', (req, res) => {
 });
 
 //Route for Admin Home Page
-router.get("/lady-lash-admin-homepage", withAuth, async (req, res) => {
+router.get('/lady-lash-admin-homepage', withAuth, async (req, res) => {
   try {
     // const selectedDate= req.query.app_date;
     // console(selected);
@@ -214,7 +214,13 @@ router.get("/lady-lash-admin-homepage", withAuth, async (req, res) => {
         appointment18: appointment18,
         appointment20: appointment20,
       }
-      res.render("adminHomepage", {availability: avaliability, appointments: appointments, date: date, logged_in: req.session.logged_in, user_id: req.session.user_id});
+      res.render("adminHomepage", {
+        availability: avaliability,
+        appointments: appointments,
+        date: date,
+        logged_in: req.session.logged_in,
+        user_id: req.session.user_id
+      });
     }
   } catch (err) {
     res.status(500).json(err);
@@ -222,7 +228,7 @@ router.get("/lady-lash-admin-homepage", withAuth, async (req, res) => {
 });
 
 //Avaliability GET
-router.get("/avaliability", async (req, res) => {
+router.get('/avaliability', async (req, res) => {
   try {
     let date = req.query.app_date;
     if (!req.query.app_date) {
@@ -240,7 +246,7 @@ router.get("/avaliability", async (req, res) => {
       console.log(day, month, year);
       date = `${year}-${month}-${day}`;
       console.log(date);
-    } 
+    }
 
     const appointmentsData = await Appointment.findAll({});
 
@@ -250,7 +256,7 @@ router.get("/avaliability", async (req, res) => {
 
     const dayAppointments = appointmentsPlain.filter(appointment => appointment.app_date === date);
     console.log(dayAppointments);
-    if(!req.session.logged_in){
+    if (!req.session.logged_in) {
       if (!dayAppointments.length) {
         const availability = {
           h08: false,
@@ -273,7 +279,7 @@ router.get("/avaliability", async (req, res) => {
         let h16 = false;
         let h18 = false;
         let h20 = false;
-  
+
         dayAppointments.forEach((appointment, i) => {
           switch (appointment.app_hour) {
             case 8:
@@ -299,7 +305,7 @@ router.get("/avaliability", async (req, res) => {
               break;
           }
         });
-  
+
         const avaliability = {
           h08: h08,
           h10: h10,
@@ -310,10 +316,13 @@ router.get("/avaliability", async (req, res) => {
           h20: h20,
         }
         console.log(avaliability);
-        res.render("avaliability", {availability: avaliability, date:date});
+        res.render("avaliability", {
+          availability: avaliability,
+          date: date
+        });
       }
 
-    }else {
+    } else {
       res.redirect("/appointment");
     }
 
@@ -328,135 +337,207 @@ router.get('/lady-lash-admin-homepage/service', (req, res) => {
 
 //Route to the new appoiment page
 router.get('/appointment', withAuth, async (req, res) => {
-  res.render('newAppointment.handlebars');
+  res.render('newAppointment.handlebars', {
+    logged_in: req.session.logged_in,
+    user_id: req.session.user_id
+  });
 });
 
 //Route to get the available hours given the selected date
 router.get('/appointment/date', withAuth, async (req, res) => {
-    try {
-      let date = req.query.app_date;
-      if (!req.query.app_date) {
-        let fullDate = new Date();
-        console.log(fullDate);
-        let day = fullDate.getDate();
-        let month = fullDate.getMonth() + 1;
-        let year = fullDate.getFullYear();
-        if (month < 10) {
-          month = "0" + month;
-        }
-        if (day < 10) {
-          day = "0" + day;
-        }
-        console.log(day, month, year);
-        date = `${year}-${month}-${day}`;
-        console.log(date);
+  try {
+    let date = req.query.app_date;
+    if (!req.query.app_date) {
+      let fullDate = new Date();
+      console.log(fullDate);
+      let day = fullDate.getDate();
+      let month = fullDate.getMonth() + 1;
+      let year = fullDate.getFullYear();
+      if (month < 10) {
+        month = "0" + month;
       }
-  
-      let showOptions= true;
+      if (day < 10) {
+        day = "0" + day;
+      }
+      console.log(day, month, year);
+      date = `${year}-${month}-${day}`;
+      console.log(date);
+    }
 
-        if (!req.query.app_date) {
-         showOptions = false;
+    let showOptions = true;
+
+    if (!req.query.app_date) {
+      showOptions = false;
+    }
+
+    const appointmentsData = await Appointment.findAll({
+      include: [{
+          model: User,
+          attributes: ["id", "first_name", "last_name", "email"]
+        },
+        {
+          model: Service,
+          attributes: ["id", "name"]
+        },
+        {
+          model: Calendar,
+          attributes: ["id", "day", "hour", "start_date", "end_date"]
+        },
+      ]
+    });
+
+    const serviceData = await Service.findAll({});
+
+    const services = serviceData.map(service => service.get({
+      plain: true
+    }));
+
+    console.log(services);
+
+    const appointmentsPlain = appointmentsData.map(appointment => appointment.get({
+      plain: true
+    }));
+
+    const dayAppointments = appointmentsPlain.filter(appointment => appointment.app_date === date);
+
+    if (!dayAppointments.length) {
+      const availability = {
+        h08: false,
+        h10: false,
+        h12: false,
+        h14: false,
+        h16: false,
+        h18: false,
+        h20: false
+      };
+      res.render("newAppointment", {
+        services: {
+          services
+        },
+        availability: availability,
+        logged_in: req.session.logged_in,
+        user_id: req.session.user_id,
+        showOptions: showOptions,
+        date: date
+      });
+    } else {
+      let h08 = false;
+      let h10 = false;
+      let h12 = false;
+      let h14 = false;
+      let h16 = false;
+      let h18 = false;
+      let h20 = false;
+
+      dayAppointments.forEach((appointment, i) => {
+        switch (appointment.app_hour) {
+          case 8:
+            h08 = true;
+            break;
+          case 10:
+            h10 = true;
+            break;
+          case 12:
+            h12 = true;
+            break;
+          case 14:
+            h14 = true;
+            break;
+          case 16:
+            h16 = true;
+            break;
+          case 18:
+            h18 = true;
+            break;
+          case 20:
+            h20 = true;
+            break;
         }
-
-      const appointmentsData = await Appointment.findAll({
-        include: [{
-            model: User,
-            attributes: ["id", "first_name", "last_name", "email"]
-          },
-          {
-            model: Service,
-            attributes: ["id", "name"]
-          },
-          {
-            model: Calendar,
-            attributes: ["id", "day", "hour", "start_date", "end_date"]
-          },
-        ]
       });
 
-      const serviceData = await Service.findAll({});
-
-      const services = serviceData.map(service => service.get({
-        plain: true
-      }));
-
-      console.log(services);
-  
-      const appointmentsPlain = appointmentsData.map(appointment => appointment.get({
-        plain: true
-      }));
-
-      const dayAppointments = appointmentsPlain.filter(appointment => appointment.app_date === date);
-
-      if (!dayAppointments.length) {
-        const availability = {
-          h08: false,
-          h10: false,
-          h12: false,
-          h14: false,
-          h16: false,
-          h18: false,
-          h20: false
-        };
-        res.render("newAppointment", {
-          services: {services},
-          availability: availability,
-          logged_in: req.session.logged_in,
-          user_id: req.session.user_id,
-          showOptions: showOptions,
-        });
-      } else {
-        let h08 = false;
-        let h10 = false;
-        let h12 = false;
-        let h14 = false;
-        let h16 = false;
-        let h18 = false;
-        let h20 = false;
-
-        dayAppointments.forEach((appointment, i) => {
-          switch (appointment.app_hour) {
-            case 8:
-              h08 = true;
-              break;
-            case 10:
-              h10 = true;
-              break;
-            case 12:
-              h12 = true;
-              break;
-            case 14:
-              h14 = true;
-              break;
-            case 16:
-              h16 = true;
-              break;
-            case 18:
-              h18 = true;
-              break;
-            case 20:
-              h20 = true;
-              break;
-          }
-        });
-
-        const avaliability = {
-          h08: h08,
-          h10: h10,
-          h12: h12,
-          h14: h14,
-          h16: h16,
-          h18: h18,
-          h20: h20,
-        }
-        
-        console.log(req.body.app_date);
-
-        res.render("newAppointment", {services : {services}, showOptions: showOptions, availability: avaliability, appointments: {dayAppointments}, logged_in: req.session.logged_in, user_id: req.session.user_id});
+      const avaliability = {
+        h08: h08,
+        h10: h10,
+        h12: h12,
+        h14: h14,
+        h16: h16,
+        h18: h18,
+        h20: h20,
       }
-    } catch (err) {
-      res.status(500).json(err);
+
+      res.render("newAppointment", {
+        services: {
+          services
+        },
+        showOptions: showOptions,
+        availability: avaliability,
+        appointments: {
+          dayAppointments
+        },
+        date: date,
+        logged_in: req.session.logged_in,
+        user_id: req.session.user_id
+      });
     }
-  });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/my-appointment', withAuth, async (req, res) => {
+  try {
+
+    console.log(req.session.user_id);
+
+    const appointmentsData = await Appointment.findAll({
+      include: {
+        model: Service
+      }
+    });
+
+    const appointments = appointmentsData.map(appointment => appointment.get({
+      plain: true
+    }));
+
+    for(let i = 0; i < appointments.length; i++) {
+      switch (appointments[i].app_hour) {
+        case 8:
+          appointments[i].app_hour = "08:00 AM";
+          break;
+        case 10:
+          appointments[i].app_hour = "10:00 AM";
+          break;
+        case 12:
+          appointments[i].app_hour = "12:00 PM";
+          break;
+        case 14:
+          appointments[i].app_hour = "02:00 PM";
+          break;
+        case 16:
+          appointments[i].app_hour = "04:00 PM";
+          break;
+        case 18:
+          appointments[i].app_hour = "06:00 PM";
+          break;
+        case 20:
+          appointments[i].app_hour = "08:00 PM";
+          break;
+      }
+    }
+
+    console.log(appointments);
+
+    res.render('userAppointments', {
+      appointments: {
+        appointments
+      },
+      logged_in: req.session.logged_in,
+      user_id: req.session.user_id
+    });
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
